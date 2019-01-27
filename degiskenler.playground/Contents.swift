2ef0bb3a4fcdd2ff5390beb0709c5f9adb2900dd
erolerme <erolerme@gmail.com>
2019-01-27 12:18:28 +0300

    Değişkenler

---
+++ degiskenler.playground/Contents.swift
@@ -0,0 +1,32 @@
+/*
+ Değişken tanımlamaları, tip dönüşümleri
+ */
+
+//Değişken Tanımlamaları
+
+var metinDegiskeni:String = "İstanbul Aydın Üniversitesi"
+
+var karakterDegiskeni:String = "H"
+
+var sayisalDegisken:Int = 4
+
+var reelSayiDegiskeni:Float = 19.2
+
+var mantiksalBooleanDegiskeni:Bool = true
+
+//Tip Dönüşümleri
+
+//Sayısal Değişkenin Metinsel Değişkene Dönüşümü
+
+var sayi = 12
+var metineDonusmusSayi:String = String(sayi)
+
+//Metinsel Değişkenin Sayısal Değişkene Dönüşümü
+
+var metin = "12"
+var sayisalaDonusmusMetin:Int? = Int(metin)
+
+//Sabit Tanımlaması
+
+let sabitSayi:Int = 12
+let sabitMetin:String = "İstanbul Aydın Üniversitesi"
